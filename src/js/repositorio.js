d7aa991f6ad843a8003ec2d5bf90597896a7d300
Eder Luis <95106788+Ederl010203@users.noreply.github.com>
2023-05-10 23:30:42 -0300

    Add files via upload

---
+++ src/js/repositorio.js
@@ -0,0 +1,39 @@
+// operadores de atribuição x = 5
+//  """"  de aritimetico x 100+50;
+ /*
+   continuação 100 -50 
+   100 / 50 
+   (100+50)* a;
+  
+
+   operadores logicos
+
+   a & b = se a for verdade e b tbm{então sera verdadeiro se não ta otimo}
+   a | b = ou[se o ar for verdadeiro e o b for falso da verdadeiro de qualquer jeito]
+   a > b = a for maior que b dar verdadeiro se não for dar falso
+   b >= a =  se for maior ou igual ao valor interno
+ loops[
+    eles são um forma de execultar o mesmo trecho de codigo.
+    
+    for (i=0; i<100;i++){( vamos flar o valor ue começao o valor ate onde vai e o valor dele para acresentar +1
+      console.log("hello world")
+    };
+     
+
+    while  execulta um bloco de codigos enquanto a valor for verdadeiro
+
+    while (i<100){
+        console.log('hello w')
+        i++;
+    }     mais simples
+]
+
+funções ele serve par agrupar codigos vocer capturar os valores digitados para o usurio e apresentar isso para ele
+functio:é a palavra reservada que avisa que ali nos vai começa a codar
+
+printMessage: poderia ser qualquer outra palavra mas é que vai dar o valor 
+message: valores que serão passda para ela
+prinm:passando a invocaçao dos valores
+
+
+*/
